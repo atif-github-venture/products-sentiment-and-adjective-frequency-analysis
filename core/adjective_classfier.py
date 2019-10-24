8d7f858bdbae144b04fe9fb9f436e7819e6e0932
aahmed <aahmed@lb.com>
2019-10-24 13:11:20 -0400

    adjective freq extraction complete

--- core/adjective_classfier.py
+++ core/adjective_classfier.py
@@ -7,7 +7,7 @@ from nltk.stem import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 import json
 
 
@@ -40,16 +40,28 @@ def extract_list_adject_to_classify(data):
                     list.append(k)
     return list
 
+def classify_adjective_from_aggr(data):
+    list = []
+    for item_list in data:
+        for key, val in item_list.items():
+            list_dict = []
+            for x in val:
+                for k, v in x.items():
+                    list_dict.append(dict({'word':k, 'freq': v, 'class': classifier(k)[0]}))
+            list.append(dict({key:list_dict}))
+    return list
+
 
 base_resource_path = os.path.join(os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)),
                                   'resources')
 revised_file = 'WomensClothing-E-Commerce-Reviews-revised.csv'
 agg_adj_freq_file = 'aggregated_adjective_per_product_freq.json'
+class_agg_adj_freq_file = 'classified_aggregated_adjective_per_product_freq.json'
 
 names = ['sino', 'Clothing ID', 'Age', 'Title', 'Review Text', 'Rating', 'Recommended IND', 'Positive Feedback Count',
          'Division Name', 'Department Name', 'Class Name', 'sentiments key']
-dataset = pandas.read_csv(base_resource_path + '/' + revised_file, nrows=15000, names=names)
-# dataset = pandas.read_csv(base_resource_path + '/' + revised_file, names=names)
+# dataset = pandas.read_csv(base_resource_path + '/' + revised_file, nrows=500, names=names)
+dataset = pandas.read_csv(base_resource_path + '/' + revised_file, names=names)
 
 dataset["sentiments key"] = dataset["sentiments key"].replace('Neutral', 'Negative')
 
@@ -94,22 +106,33 @@ print(model.score(X_test, y_test))
 
 y_test_pred = model.predict(X_test)
 print(confusion_matrix(y_test, y_test_pred))
+print(accuracy_score(y_test, y_test_pred))
 
 
 def classifier(adjective):
     return model.predict(cv.transform([adjective]))
 
 
-print(classifier('great'))
-print(classifier('less'))
-print(classifier('short'))
-print(classifier('bad'))
-
-# TODO https://www.dataschool.io/simple-guide-to-confusion-matrix-terminology/
+# print(classifier('great'))
+# print(classifier('less'))
+# print(classifier('short'))
+# print(classifier('bad'))
+#
+# # TODO https://www.dataschool.io/simple-guide-to-confusion-matrix-terminology/
+#
+# with open(base_resource_path + '/' + agg_adj_freq_file) as json_file:
+#     data = json.load(json_file)
+#     for item in extract_list_adject_to_classify(data):
+#         print(item + ': ->')
+#         print(classifier(item))
+#         print('\n')
 
 with open(base_resource_path + '/' + agg_adj_freq_file) as json_file:
     data = json.load(json_file)
-    for item in extract_list_adject_to_classify(data):
-        print(item + ': ->')
-        print(classifier(item))
-        print('\n')
+    classified_list = classify_adjective_from_aggr(data)
+json_file.close()
+
+with open(base_resource_path + '/' + class_agg_adj_freq_file, 'w') as fp:
+    # for item in per_product_freq_dist:
+    json.dump(classified_list, fp)
+fp.close()
